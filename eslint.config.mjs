e6b7e5fdfe23ecd59cd3495ba39b1b877f5b0ea3
miftahulhuda1922 <miftshul.hudaa1922@gmail.com>
2025-07-02 22:42:26 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Jika ingin tetap extend dari Next.js tapi matikan semua rules
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Override semua rule di seluruh project
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    rules: {
+      // Tambahkan ini untuk mematikan aturan secara global
+      "all": "off", // Ini symbolic, tapi ESLint tidak support "all". Maka kita matikan individual.
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@next/next/no-img-element": "off",
+      "react/react-in-jsx-scope": "off",
+      "no-console": "off",
+      "no-unused-vars": "off",
+      // ...tambahkan aturan lain sesuai yang muncul di log
+    },
+  },
 ];
 
 export default eslintConfig;
